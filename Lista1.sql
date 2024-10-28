b7ba2a707dbea397ba112e782886fbbc59e68d38
Juan Willian <134567936+JuanWillian@users.noreply.github.com>
2024-10-28 17:40:35 -0300

    Lista1.sql

--- Lista1.sql
+++ Lista1.sql
@@ -1,15 +1,18 @@
 Create database vendas;
 use vendas;
+
 create table produto(
 codigo_produto integer,
 descricao_produto varchar(30),
 preco_produto float
 )charset = utf8mb4;
+
 create table nota_fiscal(
 numero_nf integer,
 data_nf date,
 valor_nf float
 )charset = utf8mb4;
+
 create table itens(
 produto_codigo_produto Integer,
 nota_fiscal_numero_nf integer,
